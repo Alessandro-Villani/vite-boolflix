1e9be4879dfe0955d0baf2f4350a69fc37f877f1
Alessandro-Villani <alessvill@libero.it>
2023-01-26 16:14:16 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+
+    }
+  }
+
+}
+</script>
+
+<template>
+
+
+</template>
+
+<style lang="scss">
+@use './assets/styles/style.scss'
+</style>
---
+++ src/main.js
@@ -0,0 +1,19 @@
+import { createApp } from 'vue'
+//BOOTSTRAP
+import "bootstrap/dist/css/bootstrap.min.css"
+import App from './App.vue'
+//FONTAWESOME
+import {library} from '@fortawesome/fontawesome-svg-core'
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
+
+//ICONS
+import {faStar} from '@fortawesome/free-solid-svg-icons'
+import {faStar as farStar} from '@fortawesome/free-regular-svg-icons'
+library.add(faStar, farStar);
+
+
+const app = createApp(App)
+
+app.component('font-awesome-icon', FontAwesomeIcon);
+
+app.mount('#app')
